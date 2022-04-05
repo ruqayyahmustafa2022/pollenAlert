656abc2a228d768ddd331f49895875db4936fc93
RUQAYYAH MUSTAFA <ruqayyahmustafa@RUQAYYAHs-MacBook-Pro.local>
2022-04-05 14:16:12 -0400

    shell for the api access

--- src/main/java/com/detroitlabs/pollenAlert/Data/PollenData.java
+++ src/main/java/com/detroitlabs/pollenAlert/Data/PollenData.java
@@ -1,6 +1,7 @@
 package com.detroitlabs.pollenAlert.Data;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
@@ -10,4 +11,19 @@ public class PollenData {
     private String date;
     private String types;
 
+    public String getDate() {
+        return date;
+    }
+    @JsonProperty
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTypes() {
+        return types;
+    }
+    @JsonProperty
+    public void setTypes(String types) {
+        this.types = types;
+    }
 }
